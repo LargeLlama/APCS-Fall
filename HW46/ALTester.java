64150d523f4f521c18133417630c1d096d93d895
Rubin Peci <rubinpeci@yahoo.com>
2017-12-05 06:00:09 -0500

    Complete HW46

--- HW46/ALTester.java
+++ HW46/ALTester.java
@@ -1,12 +1,46 @@
+//WOOOOOOOooobin Peco
+//APCS pd. 1
+//hw46 - memes
+//2017-12-4
+//yeah
+
 import java.util.ArrayList;
 
 public class ALTester {
 	
 	public static void main(String[] args) {
 	
-		ArrayList<Integer> meme = new ArrayList();	
+		ArrayList<Comparable> meme = new ArrayList<Comparable>(); //init empty ArrayList called meme
 
 		System.out.println(meme);
+		
+		for (int i = 0; i < 23; i ++ ) //add 0 into the ArrayList 23 times
+			meme.add(0);				
+
+		System.out.println(meme);
+
+		for (int i = 0; i < meme.size(); i++) {
+			meme.set(i, (int)(Math.random() * 5) + 1);
+		}
+
+		System.out.println(meme);
+
+		//According to this, the list is not sorted when u print it - as I filled it with random ints from 1-5 and there was no order
+
+		//Testing other methods of ArrayList!
+		
+		//testing get method, printing each element with its index
+		for (int i = 0; i < meme.size(); i++) 
+			System.out.println(i + ": " + meme.get(i));
+
+		//testing remove
+
+		meme.remove(9);
+		System.out.println(meme);
+		
+		meme.add(9, 1500);
+		System.out.println(meme);
+
 	}		
 
 }

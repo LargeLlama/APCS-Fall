4f39bdc5c55aa9b3c60bcb37018b67c30f4a029a
Rubin Peci <rubinpeci@yahoo.com>
2017-12-07 23:11:34 -0500

    Complete HW49

--- HW49/OrderedArrayList.java
+++ HW49/OrderedArrayList.java
@@ -57,11 +57,56 @@ public class OrderedArrayList
 
 //should add via binary search yurd
 
-  public void add(Comparable newVal)
+  public void addBin(Comparable newVal)
   { 
-		
+	
+	if (size() == 0) {
+		_data.add(newVal);
+	}
+	else {
+	
+		int _lo = 0;	//start at first index
+		int _hi = size();	//make the highest the size
+		int currentHalf = (int)(_hi / 2);	//half has to be an int value
+
+		while (_hi > _lo) {	//as long as the _hi is larger than the _lo, it can be cut in half
+			
+			if (newVal.compareTo(_data.get(currentHalf)) < 0) {	//if the newVal is less than the currentHalf
+				_hi = currentHalf;		//set _hi to current half
+				currentHalf = (int)(_hi / 2);		//then cut half in half again
+
+			} else if (newVal.compareTo(_data.get(currentHalf)) > 0) { //if newVal is higher than the currentHalf
+				_lo = currentHalf + 1; //set _lo to half plus one
+				currentHalf = (int) (_lo + (_hi - _lo) / 2);	//sets the currentHalf to the value in between the _lo and the _hi
+			}
+			else {
+				_data.add(currentHalf, newVal);	//this only happens if they are equal, so we can just slap it in that index
+				return; 	//exit the loop
+			}
+
+		}
+		//once hi is equal to _lo or less than _lo, it runs this code
+
+		if (_hi == size())   //if its = to size, we can just add it,
+			_data.add(newVal);
+		else				//otherwise we just add it at the index _hi
+			_data.add(_hi, newVal);
+
+	}
   }
 
+
+  public void add( Comparable newVal )
+  { 
+    for( int p = 0; p < _data.size(); p++ ) {
+	    if ( newVal.compareTo( _data.get(p) ) < 0 ) { 
+        //newVal < oal[p]
+        _data.add( p, newVal );
+        return; //Q:why not break?
+	    }
+    }
+    _data.add( newVal ); //newVal > every item in oal, so add to end 
+  }
 
 
   // main method solely for testing purposes
@@ -70,15 +115,14 @@ public class OrderedArrayList
     OrderedArrayList Franz = new OrderedArrayList();
     // testing linear search
     for( int i = 0; i < 15; i++ )
-	  Franz.add( (int)( 50 * Math.random() ));
-
+    	Franz.addBin( (int)( 50 * Math.random() ));
+	System.out.println(Franz.size());
 
 
     System.out.println( Franz );
 
     //check for sorted-ness
     //if msg does not appear, list was sorted
-	Franz.remove(1);
     for( int i=0; i<Franz.size()-1; i++ ) {
       System.out.println("at index " + i + ": " + Franz.get(i) );
       if ( Franz.get(i).compareTo(Franz.get(i+1)) > 0 ) {
